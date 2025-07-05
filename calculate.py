0d83812958a73ccfccbba592725689c7c376112c
Charlotte Mathikge <charlottekmasuge@gmail.com>
2025-07-05 12:47:45 +0200

    rectangle_py file python lessons

---
+++ calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #funtion to calculate the area
+    return length * width
+
+def perimeter(length, width):
+    #Function to calculate the perimiter
+    return 2* (length + width)
